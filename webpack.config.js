8c1f85a56e210f2e5d92f086941b17e16925df4c
benyoss <benyoss4@gmail.com>
2022-01-08 15:03:20 -0600

    (add) eslint configuration.

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,29 @@
 const path = require('path');
 
 module.exports = {
-    entry: './client/src/Index.jsx',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, './client/dist'),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js|\.jsx$/,
-                loader: 'babel-loader',
-                exclude: '/node_modules',
-                options: {
-                    presets: ['@babel/preset-env', '@babel/preset-react'],
-                }
-            }, {
-                test: /\.scss|\.sass$/,
-                exclude: '/node_modules',
-                use: ['style-loader', 'sass-loader', 'css-loader'],
-            }
-        ]
-    },
-    devtool: 'eval-source-map',
-    resolve: {
-        extensions: ['.js', '.jsx'],
-    }
-}
+  entry: './client/src/Index.jsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './client/dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js|\.jsx$/,
+        loader: 'babel-loader',
+        exclude: '/node_modules',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      }, {
+        test: /\.scss|\.sass$/,
+        exclude: '/node_modules',
+        use: ['style-loader', 'sass-loader', 'css-loader'],
+      },
+    ],
+  },
+  devtool: 'eval-source-map',
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
