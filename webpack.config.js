2f13b200ac5cdf9e234c3f1c86425b5fd1087cb5
benyoss <benyoss4@gmail.com>
2022-01-09 11:43:13 -0600

    (add) bootstrap to test.

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
           presets: ['@babel/preset-env', '@babel/preset-react'],
         },
       }, {
-        test: /\.s[ac]ss$/i,
+        test: /\.(s[ac]ss|css)$/i,
         use: [
           'style-loader',
           'css-loader',
