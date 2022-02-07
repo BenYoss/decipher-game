959661fb44591eaf8ef0e019005c3420331b5880
benyoss <benyoss4@gmail.com>
2022-02-07 13:25:58 -0600

    (add) submission functionality to input.

--- client/src/components/Input.jsx
+++ client/src/components/Input.jsx
@@ -1,13 +1,38 @@
 import React, { useState } from 'react';
+import propTypes from 'prop-types';
 
-export default function Input() {
-  const { text, setText } = useState('');
+export default function Input({ text, setLevel, level }) {
+  const [val, useVal] = useState('');
   return (
     <div id="cipher-input-container">
       <form>
-        <textarea onChange={(e) => setText(e.target.value)} value={text} />
-        <button id="standard-btn" type="button" onClick={(e) => setText(e.target.value)}>Submit</button>
+        <textarea onChange={(e) => useVal(e.target.value)} />
+        <button
+          id="standard-btn"
+          type="button"
+          onClick={() => {
+            if (val === text) {
+              document.getElementsByTagName('textarea')[0].style.color = 'lime';
+              document.getElementsByTagName('textarea')[0].style.border = 'lime';
+              setLevel(level + 1);
+              setTimeout(() => {
+                document.getElementsByTagName('textarea')[0].style.color = 'black';
+                document.getElementsByTagName('textarea')[0].style.border = 'black';
+                document.getElementsByTagName('textarea')[0].value = '';
+              }, 1000);
+            }
+          }}
+        >
+          Submit
+
+        </button>
       </form>
     </div>
   );
 }
+
+Input.propTypes = {
+  text: propTypes.string.isRequired,
+  setLevel: propTypes.func.isRequired,
+  level: propTypes.string.isRequired,
+};
