03273fc2437cc2d8c72375374a52e8d32fab3938
benyoss <benyoss4@gmail.com>
2022-02-07 13:26:50 -0600

    (refactor) cipher render to handle dynamic data.

--- client/src/components/Cipher.jsx
+++ client/src/components/Cipher.jsx
@@ -1,17 +1,36 @@
 import React from 'react';
+import propTypes from 'prop-types';
 
-export default function Cipher() {
-  const body = 'The giant brown fox jumped over the lazy dog .';
+export default function Cipher({ text }) {
+  let body = text;
+  if (text) {
+    for (let i = 0; i < text.length; i += 1) {
+      if (text[i] === '.' || text[i] === '!' || text[i] === '?' || text[i] === ',') {
+        body = body.replace(text[i], ` ${text[i]}`);
+      }
+    }
+  }
   const bodyArray = body.split(' ');
   return (
     <div id="cipher-cluster">
-      {bodyArray.map((word) => (
-        <div id="cipher-word">
-          <h4 id="cipher-text">
-            {word}
-          </h4>
-        </div>
-      ))}
+      {text.length ? (
+        bodyArray.map((word) => (
+          <div id="cipher-word">
+            <h4 id="cipher-text">
+              {word}
+            </h4>
+          </div>
+        ))) : (
+          <div id="no-cipher">
+            <h4>Congrats!</h4>
+            <h6>You have completed all of the available ciphers for today!</h6>
+            <h6>Stay tuned for more ciphers tomorrow!</h6>
+          </div>
+      )}
     </div>
   );
 }
+
+Cipher.propTypes = {
+  text: propTypes.string.isRequired,
+};
