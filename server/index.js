508c12ba0c11fb59b781ff54ee0d8643bc135217
benyossingest <ben@ingest.ai>
2022-03-08 17:27:16 -0600

    (add) app router to handle web cookies.

--- server/index.js
+++ server/index.js
@@ -1,15 +1,28 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const path = require('path');
+const cookieRoute = require('./routes/cookie');
+const appRoute = require('./routes/app');
 
 const dist = path.resolve(__dirname, '../client/dist');
 const app = express();
 
+/**
+ * TODO: set up cookie data here:
+ */
+app.use(cookieParser());
+app.use(cookieRoute);
+
+app.use(appRoute);
 app.use(express.static(dist));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(dist, 'index.html'));
 });
 
+/**
+ *  Server listener to start server.
+ */
 app.listen('8080', () => {
   console.log('Server has successfully connected! 🚀🚀 \nhttp://localhost:8080');
 });
